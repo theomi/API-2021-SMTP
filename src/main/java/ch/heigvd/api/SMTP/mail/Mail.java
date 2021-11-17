0297df58da8bbf317a96dccf88fef690f6251e91
Théo <theo.mirabile@me.com>
2021-11-17 11:21:36 +0100

    [done] added mail base structure

--- src/main/java/ch/heigvd/api/SMTP/mail/Mail.java
+++ src/main/java/ch/heigvd/api/SMTP/mail/Mail.java
@@ -1,4 +1,39 @@
 package ch.heigvd.api.SMTP.mail;
 
 public class Mail {
+
+    private String from;
+    private String to;
+    private String cc;
+
+    private String subject;
+    private String body;
+
+    public Mail(String from, String to, String cc, String subject, String body) {
+        this.from = from;
+        this.to = to;
+        this.cc = cc;
+        this.subject = subject;
+        this.body = body;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public String getCc() {
+        return cc;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public String getBody() {
+        return body;
+    }
 }
